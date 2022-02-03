7f87891bd2ae8231461371c8a19739f137d5d99f
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-03 19:02:21 +0100

    Add placeholder to buttons and change font of project name

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,8 +2,16 @@ export default function Meme() {
   return (
     <div className="form">
       <div className="form__inputs">
-        <input type="text" className="form__inputs__field" />
-        <input type="text" className="form__inputs__field" />
+        <input
+          type="text"
+          className="form__inputs__field"
+          placeholder="Top text"
+        />
+        <input
+          type="text"
+          className="form__inputs__field"
+          placeholder="Bottom text"
+        />
       </div>
       <button className="form__btn">Get a new meme image 🖼️</button>
     </div>
